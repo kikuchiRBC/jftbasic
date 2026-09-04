6c142140e89d9b19d7242b702a98aaceb3deaf1d
kikuchiRBC <kikuchi@rbc.ac.jp>
2026-09-04 16:58:29 +0900

    Add files via upload

---
+++ js/quizData3.js
@@ -0,0 +1,503 @@
+let allImageQuizData = [
+    {image:"imgs/1.jpg",
+  answer:"前",
+  choices:["前","上","下","中"
+    ]},
+    
+{image:"imgs/2.jpg",
+  answer:"下",
+  choices:["前","上","下","中"
+    ]},
+{image:"imgs/3.jpg",
+  answer:"中",
+  choices:["前","上","下","中"
+    ]},
+{image:"imgs/4.jpg",
+  answer:"横",
+  choices:["前","上","下","横"
+    ]},
+{image:"imgs/5.jpg",
+  answer:"後ろ",
+  choices:["前","後ろ","下","横"
+    ]},
+
+    //651
+    {image:"imgs/ilust/onomatope/1.png",
+    question:"絵にあうことばをえらびなさい",
+  answer:"ほえている",
+  
+  choices:["ほえている","わらっている","おこっている","ねている"
+    ]},
+    {image:"imgs/ilust/onomatope/2.png",
+  answer:"サプライズ",
+  choices:["ざんねん","ショック","サプライズ","イライラ"
+    ]},
+
+{image:"imgs/ilust/onomatope/3.png",
+  answer:"うれしい",
+  choices:["ざんねん","ショック","うれしい","イライラ"
+    ]},
+{image:"imgs/ilust/onomatope/4.png",
+  answer:"クイズ",
+  choices:["クイズ","ピカピカ","うれしい","イライラ"
+    ]},
+{image:"imgs/ilust/onomatope/5.png",
+  answer:"ショック",
+  choices:["ピカピカ","ショック","シーン","イライラ"
+    ]},
+{image:"imgs/ilust/onomatope/6.png",
+  answer:"ひらめいた",
+  choices:["クイズ","シーン","ひらめいた","イライラ"
+    ]},
+{image:"imgs/ilust/onomatope/7.png",
+  answer:"ざんねん",
+  choices:["クイズ","ざんねん","ひらめいた","イライラ"
+    ]},
+{image:"imgs/ilust/onomatope/8.png",
+  answer:"ぴゅーぴゅー",
+  choices:["ぴゅーぴゅー","シーン","ゴロゴロ","イライラ"
+    ]},
+{image:"imgs/ilust/onomatope/9.png",
+  answer:"ゴロゴロ",
+  choices:["ピカピカ","ショック","シーン","ゴロゴロ"
+    ]},
+    
+{image:"imgs/ilust/onomatope/10.png",
+  answer:"ピカピカ",
+  choices:["シーン","ゴロゴロ","ひらめいた","ピカピカ"
+    ]},
+{image:"imgs/ilust/onomatope/11.png",
+  answer:"イライラ",
+  choices:["シーン","ゴロゴロ","イライラ","ピカピカ"
+    ]},
+{image:"imgs/ilust/onomatope/12.png",
+  answer:"たのしみ",
+  choices:["たのしみ","ざんねん","ひらめいた","ショック"
+    ]},
+{image:"imgs/ilust/onomatope/13.png",
+  answer:"ざんねん",
+  choices:["たのしみ","ざんねん","ひらめいた","うれしい"
+    ]},
+{image:"imgs/ilust/onomatope/14.png",
+  answer:"ザーザー",
+  choices:["トントン","ワンワン","ザーザー","うれしい"
+    ]},
+    {image:"imgs/ilust/onomatope/15.png",
+  answer:"ぽつぽつ",
+  choices:["ちゃんちゃん","ワンワン","ぽつぽつ","うれしい"
+    ]},
+    {image:"imgs/ilust/onomatope/16.png",
+  answer:"しとしと",
+  choices:["ゴロゴロ","ワンワン","しとしと","ちゃんちゃん"
+    ]},    
+    {image:"imgs/ilust/onomatope/17.png",
+  answer:"シーン",
+  choices:["シーン","ガーン","パーン","ちゃんちゃん"
+    ]},     {image:"imgs/ilust/2.png",
+  answer:"すもう",
+  choices:["すもう","ラグビー","サッカー","バスケットボール"
+    ]},  
+       {image:"imgs/ilust/3.png",
+  answer:"山登り",
+  choices:["山登り","山落とし","川落とし","川登り"
+    ]},  
+           {image:"imgs/ilust/4.png",
+  answer:"サッカー",
+  choices:["すもう","ラグビー","サッカー","バスケットボール"
+    ]},    
+           {image:"imgs/ilust/5.png",
+  answer:"コンサート",
+  choices:["コンサート","試合（しあい）","サッカー","天気"
+    ]},  
+           {image:"imgs/ilust/6.png",
+  answer:"演劇（えんげき）",
+  choices:["演劇（えんげき）","試合（しあい）","サッカー","天気"
+    ]},      
+           {image:"imgs/ilust/7.png",
+  answer:"試合（しあい）",
+  choices:["コンサート","試合（しあい）","サッカー","山登り"
+    ]}, 
+           {image:"imgs/ilust/8.png",
+  answer:"ライブ",
+  choices:["ライブ","試合（しあい）","サッカー","天気"
+    ]},
+          {image:"imgs/ilust/9.png",
+  answer:"ラグビー",
+  choices:["演劇（えんげき）","ラグビー","サッカー","バスケットボール"
+    ]},   
+         {image:"imgs/ilust/10.png",
+  answer:"演劇（えんげき）",
+  choices:["演劇（えんげき）","ラグビー","サッカー","バスケットボール"
+    ]}, 
+         {image:"imgs/ilust/11.png",
+  answer:"お祭り",
+  choices:["お祭り","スーパー","コンビニ","そうこ"
+    ]}, 
+         {image:"imgs/ilust/12.png",
+  answer:"レストラン",
+  choices:["レストラン","スーパー","コンビニ","そうこ"
+    ]}, 
+         {image:"imgs/ilust/13.png",
+  answer:"おしゃれな町",
+  choices:["たこくせきな町","おしゃれな町","かなしい町","おこる町"
+    ]}, 
+         {image:"imgs/ilust/14.png",
+  answer:"たこくせきな町",
+  choices:["たこくせきな町","こわい町","かなしい町","おこる町"
+    ]}, 
+         {image:"imgs/ilust/15.png",
+  answer:"しょみんてきな町",
+  choices:["しょみんてきな町","こわい町","かなしい町","おこる町"
+    ]}, 
+       {image:"imgs/ilust/16.png",
+  answer:"ひっこし",
+  choices:["ひっこし","そつぎょう","走る","食べる"
+    ]}, 
+           {image:"imgs/ilust/17.png",
+  answer:"日当たり（ひあたり）",
+  choices:["日当たり（ひあたり）","夜","階段","びっくり"
+    ]}, 
+              {image:"imgs/ilust/18.png",
+  answer:"エアコン を つける",
+  choices:["エアコン を つける","エアコン を つきる","リモコン を わたす","リモコン を きる"
+    ]}, 
+              {image:"imgs/ilust/19.png",
+  answer:"かんきせん を つける",
+  choices:["かんきせん を つける","かんきせん を つきる","せんぷうき を わたす","せんぷうき を つける"
+    ]}, 
+                {image:"imgs/ilust/20.png",
+  answer:"まど が われた",
+  choices:["まど が われた","まど が わりた","かがみ が われた","かがみ が わりた"
+    ]}, 
+    
+                    {image:"imgs/ilust/21.png",
+  answer:"でんき が つかない",
+  choices:["でんき が つかない","でんき が つきない","でんち が つかない","でんち が つきない"
+    ]}, 
+        
+                    {image:"imgs/ilust/22.png",
+  answer:"水 が ながれない",
+  choices:["水 が ながれない","水 が なげれない","水 が おちない","水 が おつない"
+    ]}, 
+    
+                        {image:"imgs/ilust/23.png",
+  answer:"水 が でてこない",
+  choices:["水 が でてこない","水 が でとこない","水 が おちない","水 が おつない"
+    ]}, 
+    
+                 {image:"imgs/ilust/24.png",
+  answer:"お湯 が でてこない",
+  choices:["水 が でてこない","水 が でとこない","お湯 が でてこない","お湯 が でとこない"
+    ]}, 
+        
+                 {image:"imgs/ilust/25.png",
+  answer:"水 が もれている",
+ choices:["水 が もれている","水 が もりている","水 が おちない","水 が おつない"
+    ]},          
+          {image:"imgs/ilust/26.png",
+  answer:"かぎ を なくした",
+ choices:["かぎ を なくした","かぎ を なけした","かげ を なくした","かげ を なきした"
+    ]}, 
+         {image:"imgs/ilust/27.png",
+  answer:"ワイファイ が つながらない",
+ choices:["ワイファイ が つながらない","ワイファイ が つなぎない","ワイファイ が つかない","ワイファイ が つきない"
+    ]}, 
+             {image:"imgs/ilust/28.png",
+  answer:"音 が うるさい",
+ choices:["音 が うるさい","音 を うるさい","音 が すてき","音 を すてき"
+    ]}, 
+
+              {image:"imgs/ilust/29.png",
+  answer:"注文（ちゅうもん）する",
+ choices:["注文（ちゅうもん）する","注文（ちゅうもん）へる","注意（ちゅうい）する","注意（ちゅうい）へる"
+    ]}, 
+    
+            {image:"imgs/ilust/30.png",
+  answer:"ラーメン",
+ choices:["うどん","ラーメン","そば","そうめん"
+    ]}, 
+        
+         {image:"imgs/ilust/31.png",
+  answer:"ハンバーグ",
+ choices:["ハンバーグ","ラーメン","そば","そうめん"
+    ]}, 
+    {image:"imgs/ilust/33.png",
+  answer:"フライドポテト",
+ choices:["ハンバーグ","フライドポテト","そば","そうめん"
+    ]}, 
+     {image:"imgs/ilust/34.png",
+  answer:"パフェ",
+ choices:["ハンバーグ","フライドポテト","パフェ","そうめん"
+    ]}, 
+     {image:"imgs/ilust/35.png",
+  answer:"居酒屋（いざかや）",
+ choices:["居酒屋（いざかや）","トイレ","リビング","スーパー"
+    ]}, 
+     {image:"imgs/ilust/36.png",
+  answer:"食堂（しょくどう）",
+ choices:["食堂（しょくどう）","トイレ","リビング","スーパー"
+    ]}, 
+     {image:"imgs/ilust/37.png",
+  answer:"にんじん",
+ choices:["にんじん","じゃがいも","みかん","メロン"
+    ]}, 
+     {image:"imgs/ilust/38.png",
+  answer:"だいこん",
+ choices:["にんじん","じゃがいも","だいこん","メロン"
+    ]}, 
+     {image:"imgs/ilust/39.png",
+  answer:"だいず",
+ choices:["にんじん","ピーナッツ","だいず","メロン"
+    ]}, 
+     {image:"imgs/ilust/40.png",
+  answer:"ピーナッツ",
+ choices:["にんじん","ピーナッツ","だいず","メロン"
+    ]}, 
+     {image:"imgs/ilust/41.png",
+  answer:"みそ",
+ choices:["あぶらあげ","とうふ","みそ","メロン"
+    ]}, 
+     {image:"imgs/ilust/42.png",
+  answer:"あぶらあげ",
+ choices:["あぶらあげ","とうふ","まめ","メロン"
+    ]}, 
+     {image:"imgs/ilust/43.png",
+  answer:"ちょうみりょう",
+ choices:["ちょうみりょう","ガソリン","せんざい","コーヒー"
+    ]}, 
+     {image:"imgs/ilust/44.png",
+  answer:"こうたい",
+ choices:["こうたい","せんたい","こうしん","せんざい"
+    ]}, 
+     {image:"imgs/ilust/45.png",
+  answer:"デリバリー",
+ choices:["デリバリー","テリトリー","デリシャス","バッテリー"
+    ]}, 
+     {image:"imgs/ilust/46.png",
+  answer:"あらいもの",
+ choices:["あらいもの","せんたくもの","おちょうしもの","くだもの"
+    ]}, 
+     {image:"imgs/ilust/47.png",
+  answer:"しょうぎ",
+ choices:["しょうぎ","トランプ","オセロ","しんけいすいじゃく"
+    ]}, 
+     {image:"imgs/ilust/48.png",
+  answer:"うたう",
+ choices:["うたう","はしる","とまる","おちる"
+    ]}, 
+         {image:"imgs/ilust/49.png",
+  answer:"おんせん",
+ choices:["おんせん","おんねん","だんせん","しんせん"
+    ]}, 
+       {image:"imgs/ilust/50.png",
+  answer:"おばあさん",
+ choices:["おばあさん","おじいさん","こども","まご"
+    ]}, 
+        {image:"imgs/ilust/51.png",
+  answer:"バスてい",
+ choices:["バスてい","でんしゃ","ちゅうしゃじょう","くるま"
+    ]}, 
+      {image:"imgs/ilust/52.png",
+  answer:"ゴミすて",
+ choices:["ゴミすて","かみすて","ばすてと","こうして"
+    ]}, 
+      {image:"imgs/ilust/53.png",
+  answer:"じゅぎょう を うける",
+ choices:["じゅぎょう を うける","じゅぎょう を すてる","じゅぎょう を はしる","じゅぎょう を こる"
+    ]}, 
+      {image:"imgs/ilust/54.png",
+  answer:"きたない",
+ choices:["きたない","きれい","たのしい","おもしろい"
+    ]}, 
+        {image:"imgs/ilust/55.png",
+  answer:"におい",
+ choices:["におい","きれい","かおり","おもしろい"
+    ]}, 
+            {image:"imgs/ilust/56.png",
+  answer:"じゃま",
+ choices:["じゃま","くさい","かおり","とおれる"
+    ]}, 
+      {image:"imgs/ilust/57.png",
+  answer:"はさみ で きる",
+ choices:["はさみ で きる","はさみ ん きる","はさみ が きる","はさみ は きる"
+    ]},
+          {image:"imgs/ilust/58.png",
+  answer:"かみ で つつむ",
+ choices:["かみ で つつむ","かみ ん つつむ","かみ が つつむ","かみ は つつむ"
+    ]},
+      {image:"imgs/ilust/59.png",
+  answer:"かたい",
+ choices:["かたい","あつい","やわらかい","さむい"
+    ]},
+      {image:"imgs/ilust/60.png",
+  answer:"かぞく と たべる",
+ choices:["かぞく と たべる","かぞく に たべる","かぞく ぞ たべる","かぞく し たべる"
+    ]},
+          {image:"imgs/ilust/61.png",
+  answer:"ハイキング",
+ choices:["ハイキング","カイキング","コイキング","タイキング"
+    ]},
+     {image:"imgs/ilust/62.png",
+  answer:"たばこ を すっています",
+ choices:["たばこ を すっています","たばこ に すっています","たばこ が すっています","たばこ だ すっています"
+    ]},
+     {image:"imgs/ilust/63.png",
+  answer:"でんしゃ に のっています",
+ choices:["でんしゃ に のっています","でんしゃ に おりています","でんしゃ が のっています","でんしゃ ん のっています"
+    ]},
+     {image:"imgs/ilust/64.png",
+  answer:"けむり が でています",
+ choices:["けむり が でています","けむり に でています","けむり ぎ でています","けむり こ でています"
+    ]},
+      {image:"imgs/ilust/65.png",
+  answer:"火 が でています",
+ choices:["火 が でています","火 に でています","火 ぎ でています","火 こ でています"
+    ]},
+      {image:"imgs/ilust/66.png",
+  answer:"家 が もえています",
+ choices:["家 が もえています","家 が ぬれています","家 が さめています","家 が かれています"
+    ]},
+     {image:"imgs/ilust/67.png",
+  answer:"けっこんしき",
+ choices:["けっこんしき","にゅうがくしき","そつぎょうしき","こうしき"
+    ]},
+      {image:"imgs/ilust/68.png",
+  answer:"石",
+ choices:["石","火","水","神"
+    ]},
+       {image:"imgs/ilust/69.png",
+  answer:"紙",
+ choices:["紙","火","水","神"
+    ]},
+          {image:"imgs/ilust/70.png",
+  answer:"はさみ",
+ choices:["はさみ","カッター","のり","ペン"
+    ]},
+         {image:"imgs/ilust/71.png",
+  answer:"テレビ を みています",
+ choices:["テレビ を みています","ラジオ を きいています","うんどう を しています","ごはん を たべています"
+    ]},
+        {image:"imgs/ilust/72.png",
+  answer:"けしょう が こい",
+ choices:["けしょう が こい","うんどう が こい","たいそう が こい","かいきん が こい"
+    ]},
+       {image:"imgs/ilust/73.png",
+  answer:"ジュース を のんでいます",
+ choices:["ジュース を のんでいます","あるいています","うんどう を しています","ごはん を たべています"
+    ]},
+       {image:"imgs/ilust/74.png",
+  answer:"ぬすまれた",
+ choices:["ぬすまれた","たべられた","かけられた","じられた"
+    ]},
+       {image:"imgs/ilust/75.png",
+  answer:"せいぞう",
+ choices:["せいぞう","はんばい","こうつう","せっきゃく"
+    ]},
+       {image:"imgs/ilust/76.png",
+  answer:"まいご",
+ choices:["まいご","けいご","たいご","がいご"
+    ]},
+        {image:"imgs/ilust/77.png",
+  answer:"せいかつ",
+ choices:["せいかつ","けいかつ","たいかつ","がいかつ"
+    ]},
+        {image:"imgs/ilust/78.png",
+  answer:"ゴミ を すてましょう",
+ choices:["ゴミ を すてましょう","スマホ を つかいましょう","かみ を きりましょう","たべましょう"
+    ]},
+        {image:"imgs/ilust/79.png",
+  answer:"ちゅうりんじょう",
+ choices:["ちゅうりんじょう","ちゅうしゃじょう","じょうしゃじょう","こうじょう"
+    ]},
+           {image:"imgs/ilust/80.png",
+  answer:"そつぎょうしき",
+ choices:["そつぎょうしき","にゅうがくしき","かいしき","じょうしき"
+    ]},
+
+
+    //C
+       {image:"imgs/ilust/81.png",
+  answer:"けしき",
+ choices:["けしき","こうじ","でぐち","すきやき"
+    ]},
+      {image:"imgs/ilust/82.png",
+  answer:"ピアス",
+ choices:["ピアス","ゆびわ","うでわ","バッジ"
+    ]},
+      {image:"imgs/ilust/83.png",
+  answer:"アクセサリー",
+ choices:["アクセサリー","くつ","ほうき","たばこ"
+    ]},
+      {image:"imgs/ilust/84.png",
+  answer:"サンダル",
+ choices:["サンダル","ハイヒール","パンプス","スニーカー"
+    ]},
+     {image:"imgs/ilust/85.png",
+  answer:"ぶつかった",
+ choices:["ぶつかった","こおった","きった","かわった"
+    ]},
+      {image:"imgs/ilust/86.png",
+  answer:"でんとうぶんか",
+ choices:["でんとうぶんか","かいしょく","でんきぶんか","こうとうぶんか"
+    ]},
+       {image:"imgs/ilust/87.png",
+  answer:"しゃしん を とります",
+ choices:["しゃしん を とります","チーズ を たべます","スマホ を みます","けが を しています"
+    ]},
+    {image:"imgs/ilust/88.png",
+  answer:"おそうしき",
+ choices:["おそうしき","そつぎょうしき","にゅうがくしき","にゅうしゃしき"
+    ]},
+     {image:"imgs/ilust/89.png",
+  answer:"わかいおんなのひと",
+ choices:["わかいおんなのひと","ふるいおんなのひと","わかいおとこのひと","ふるいおとこのひと"
+    ]},
+      {image:"imgs/ilust/90.png",
+  answer:"うるさい",
+ choices:["うるさい","しずか","おなかいっぱい","おなかすいた"
+    ]},
+       {image:"imgs/ilust/91.png",
+  answer:"にゅういん",
+ choices:["にゅういん","にゅうこう","こうにゅう","とうにゅう"
+    ]},
+       {image:"imgs/ilust/92.png",
+  answer:"つかれた",
+ choices:["つかれた","おいしかった","ありがとうございました","のぼせた"
+    ]},
+        {image:"imgs/ilust/93.png",
+  answer:"あめ が ふっている",
+ choices:["あめ が ふっている","おこめ を たく","じてんしゃ に のる","たいよう が のぼる"
+    ]},
+     {image:"imgs/ilust/94.png",
+  answer:"かみ が ながい",
+ choices:["かみ が ながい","ひげ が ながい","みみ が ながい","まゆげ が ながい"
+    ]},
+     {image:"imgs/ilust/95.png",
+  answer:"ざんぎょう",
+ choices:["ざんぎょう","ようほう","とうほう","こうぼう"
+    ]},
+     {image:"imgs/ilust/96.png",
+  answer:"はで な かみ",
+ choices:["はで な かみ","じみ な かみ","はで な みみ","じみ な みみ"
+    ]},
+     {image:"imgs/ilust/97.png",
+  answer:"かいぎ",
+ choices:["かいぎ","そうじ","おしょくじ","りょうり"
+    ]},
+      {image:"imgs/ilust/98.png",
+  answer:"つくえ を はこぶ",
+ choices:["つくえ を はこぶ","いす を はこぶ","つくえ を なでる","いす を なでる"
+    ]},
+       {image:"imgs/ilust/99.png",
+  answer:"ふね",
+ choices:["ふね","トロッコ","でんしゃ","エレベーター"
+    ]},
+
+ {image:"imgs/ilust/100.png",
+  answer:"はんばい",
+ choices:["はんばい","こうばん","かいばん","けいばん"
+]},
+
+
+
+];
